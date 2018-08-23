d034d9da795658efc64d2724a414a2af2a229723
Yanghwa <kellerj87@hotmail.com>
2018-08-23 09:37:12 -0400

    Sending Email

--- src/shared/baseurl.ts
+++ src/shared/baseurl.ts
@@ -1,3 +1,3 @@
-// export const baseURL = 'http://206.55.87.88:3000/';  //company ip
+export const baseURL = 'http://206.55.87.88:3000/';  //company ip
 
-export const baseURL = 'http://localhost:3000/';
+// export const baseURL = 'http://localhost:3000/';
