690b580e6f833372642e5cbf9ba8df324c8859de
Yanghwa <kellerj87@hotmail.com>
2018-08-23 10:00:17 -0400

    Social Sharing

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -4,6 +4,7 @@ import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { CommentPage } from '../../pages/comment/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 /**
  * Generated class for the DishdetailPage page.
@@ -27,7 +28,7 @@ export class DishdetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, @Inject('BaseURL') private BaseURL, 
   private favoriteservice: FavoriteProvider, private toastCtrl: ToastController, 
-  private actionSheetCtrl: ActionSheetController, private modalCtrl: ModalController) {
+  private actionSheetCtrl: ActionSheetController, private modalCtrl: ModalController, private socialSharing: SocialSharing) {
     this.dish = navParams.get('dish');
     this.numcomments = this.dish.comments.length;
 
@@ -66,6 +67,20 @@ export class DishdetailPage {
           handler: () => {
             console.log('comment clicked');
             this.openAddComment();
+          }
+        },{
+          text: 'Share via Facebook',
+          handler: () => {
+            this.socialSharing.shareViaFacebook(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Facebook'))
+              .catch(() => console.log('Failed to post to Facebook'));
+          }
+        },{
+          text: 'Share via Twitter',
+          handler: () => {
+            this.socialSharing.shareViaTwitter(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Twitter'))
+              .catch(() => console.log('Failed to post to Twitter'));
           }
         },{
           text: 'Cancel',
